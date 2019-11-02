23ba29fdbaeabb9fa01b206081e7e412aa81ed15
prmejitbenake <benakepremjit@gmail.com>
2019-11-02 09:38:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class App extends Component {
+  render() {
+    return <div>hello from app</div>;
+  }
+}
